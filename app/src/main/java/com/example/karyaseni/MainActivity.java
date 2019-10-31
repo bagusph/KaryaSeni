9601744c835c01848fb65f2d4ee1c340dfefc2fd
bagusph <bagus.pogar@gmail.com>
2019-10-31 16:41:11 +0700

    KaryaSeniVersi1

---
+++ app/src/main/java/com/example/karyaseni/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.karyaseni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int waktu_loading=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home=new Intent(MainActivity.this, Home.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
